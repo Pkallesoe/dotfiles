57ac0db7be2a9ba40d943b932a792c630586566b
Peter Schmidt-Kallesøe <pkallesoe@gmail.com>
2014-03-09 13:15:11 +0100

    Added gtkrc-2.0

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Add the files that needs symlinks
-files="Xresources profile tmux.conf vimrc xinitrc zshrc"
+files="Xresources profile tmux.conf vimrc xinitrc zshrc gtkrc-2.0"
 
 for file in $files; do
     ln -s ~/dotfiles/$file ~/.$file
