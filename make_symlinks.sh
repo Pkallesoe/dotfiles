cd87169de545207947d28e114441190585a4316e
Peter Schmidt-Kallesøe <pkallesoe@gmail.com>
2014-03-09 12:23:52 +0100

    Fixed xinitrc not loading fonts and fixed fault in make_symlinks.sh

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Add the files that needs symlinks
-files="Xresources profile tmux_conf vimrc xinitrc zshrc"
+files="Xresources profile tmux.conf vimrc xinitrc zshrc"
 
 for file in $files; do
     ln -s ~/dotfiles/$file ~/.$file
 done
-echo "...Done!
+echo "...Done!"
